c525a16fa2c42485db4e94c9f1229bed196164af
Norbi <ibrone@gmail.com>
2021-12-01 10:22:25 +0100

    Initial commit

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga_pot_potvizsga/NumberOfDigits/NumberOfDigits.java
@@ -0,0 +1,4 @@
+package hu.nive.ujratervezes.kepesitovizsga_pot_potvizsga.NumberOfDigits;
+
+public class NumberOfDigits {
+}
---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga_pot_potvizsga/uppercase/UpperCaseLetters.java
@@ -0,0 +1,4 @@
+package hu.nive.ujratervezes.kepesitovizsga_pot_potvizsga.uppercase;
+
+public class UpperCaseLetters {
+}
