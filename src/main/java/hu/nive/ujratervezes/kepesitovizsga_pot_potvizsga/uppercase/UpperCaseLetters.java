207313935063db242626a1694638ab142fb33da3
Norbi <ibrone@gmail.com>
2021-12-01 11:24:16 +0100

    commit

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga_pot_potvizsga/uppercase/UpperCaseLetters.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga_pot_potvizsga/uppercase/UpperCaseLetters.java
@@ -1,4 +1,30 @@
 package hu.nive.ujratervezes.kepesitovizsga_pot_potvizsga.uppercase;
 
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+
 public class UpperCaseLetters {
+    public static void main(String[] args) {
+        RandomAccessFile raf;
+        String sor;
+        ArrayList<String> characters = new ArrayList<String>();
+
+        try {
+            raf = new RandomAccessFile("characters.txt", "r");
+
+            for (sor = raf.readLine(); sor != null; sor = raf.readLine()) {
+                
+                characters.toLowerCase();
+            }
+
+            raf.close();
+        } catch (IOException e) {
+            System.out.println("HIBA");
+        }
+    }
+
+
 }
